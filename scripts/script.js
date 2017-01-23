c756b3876f537c597c5dade9b267920da7a44ccb
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-01-23 11:40:28 -0800

    Initial grid created

---
+++ scripts/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    for(var x = 0; x < 16; x++) {
+        for(var y = 0; y < 16; y++) {
+            var unit = $("<div class='unit'></div>");
+            unit.appendTo('#container');
+        }
+    }
+});
