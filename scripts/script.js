d76b7397f1e5e091ef8d702299d5bb3a43abf487
Tim Lapinskas <tim.lapinskas@gmail.com>
2017-01-23 14:16:34 -0800

    Completed jQuery and CSS

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,39 @@
+// function that creates grid when page loads and allows mouseover function
 $(document).ready(function() {
-    for(var x = 0; x < 16; x++) {
-        for(var y = 0; y < 16; y++) {
-            var unit = $("<div class='unit'></div>");
-            unit.appendTo('#container');
-        }
-    }
+    createGrid(50);
+
+    $(".box").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
+
+    $(".refreshGrid").click(function() {
+        refresh();
+
+        $(".box").mouseover(function() {
+        $(this).css("background-color", "black");
+        });
+    });
 });
+
+// builds a grid in the "container"
+function createGrid(x) {
+    for (var rows = 0; rows < x; rows++) {
+        for (var columns = 0; columns < x; columns++) {
+            $("#container").append("<div class='box'></div>");
+        };
+    };
+    $(".box").width(960/x);
+    $(".box").height(960/x);
+};
+
+// function that clears the grid
+function clearGrid() {
+	$(".box").remove();
+};
+
+// funciton to refresh the grid and ask user how many boxes in the new grid
+function refresh() {
+	var x = prompt("How many squares per side? Default is 50.")
+	clearGrid();
+	createGrid(x);
+}
